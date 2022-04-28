e396cc3be931587b5adf6c22c7417f75b79a685a
Mehdi Haghgoo <powergame_coder2@yahoo.com>
2022-04-28 19:45:31 -0400

    Replaced deprecated components from react-router-dom with new ones

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import {BrowserRouter as Router, Route, Navigate, Routes } from 'react-router-dom'
+// import {Router, Route, Redirect } from 'react-router-dom'
 import NavBar from './components/navbar/NavBar';
 import Teams from './components/pages/teams/Teams';
 import Calendar from './components/pages/calendar/Calendar';
@@ -15,14 +16,14 @@ class App extends Component {
     return (
       <Router>
         <NavBar />
-        <Switch>
-          <Route path="/" exact component={() => <Redirect to="/teams" />} />
+        <Routes>
+          <Route path="/" exact component={() => <Navigate to="/teams" />} />
           <Route path="/teams" component={Teams} />
           <Route path="/calendar" component={Calendar} />
           <Route component={() => (
             <Error message="We couldn't find what you're looking for." />
           )}/>
-        </Switch>
+        </Routes>
         <Footer />
       </Router>
     );
